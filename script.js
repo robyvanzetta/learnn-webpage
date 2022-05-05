03c5320f93b379e6a1b4eb4096cb8aa15142a4e9
Roberto <roby.vanzetta@gmail.com>
2022-05-05 15:29:37 +0200

    backBtn

---
+++ script.js
@@ -0,0 +1,15 @@
+const backBtn = document.getElementById('backBtn');
+
+backBtn.addEventListener('click', function() {
+    window.scrollTo(0, 0);
+});
+
+window.addEventListener('scroll', function() {
+    let posY = window.scrollY;
+
+    if (posY > 0) {
+        backBtn.style.display = 'block'
+    } else {
+        backBtn.style.display ='none'
+    }
+});
